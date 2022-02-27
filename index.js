ae2b60b56288091acb17632da1a45688430d745d
root <saung.myat@gmail.com>
2022-02-26 21:33:56 -0500

    commit tree 380e99cecd9de553208f38860f47f62ef5c03e78

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+multiply;
+
+const random = Math.floor(Math.random() * 10) + 1;
+random;
+
+const num3 = 34;
+const num4 = 5;
+const mod = num3 % num4;
+mod;
+
+const max = Math.max(2, 3, 6, 12, 4, 16, 20);
+max;
